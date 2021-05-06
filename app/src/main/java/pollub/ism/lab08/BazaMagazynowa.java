c58c727106f9c0699019532f3e54a3f3752c95a1
Rafał Wróbel <wrobel1522@gmail.com>
2021-05-06 10:49:20 +0200

    Lab08 wersja do wglądu

---
+++ app/src/main/java/pollub/ism/lab08/BazaMagazynowa.java
@@ -0,0 +1,13 @@
+package pollub.ism.lab08;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {pollub.ism.lab08.PozycjaMagazynowa.class}, version = pollub.ism.lab08.BazaMagazynowa.WERSJA, exportSchema = false)
+public abstract class BazaMagazynowa extends RoomDatabase {
+
+    public static final String NAZWA_BAZY = "Stoisko z warzywami";
+    public static final int WERSJA = 1;
+
+    public abstract pollub.ism.lab08.PozycjaMagazynowaDAO pozycjaMagazynowaDAO();
+}
---
+++ app/src/main/java/pollub/ism/lab08/PozycjaMagazynowaDAO.java
@@ -0,0 +1,25 @@
+package pollub.ism.lab08;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface PozycjaMagazynowaDAO {
+
+    @Insert  //Automatyczna kwerenda wystarczy
+    public void insert(pollub.ism.lab08.PozycjaMagazynowa pozycja);
+
+    @Update //Automatyczna kwerenda wystarczy
+    void update(pollub.ism.lab08.PozycjaMagazynowa pozycja);
+
+    @Query("SELECT QUANTITY FROM Warzywniak WHERE NAME= :wybraneWarzywoNazwa") //Nasza kwerenda
+    int findQuantityByName(String wybraneWarzywoNazwa);
+
+    @Query("UPDATE Warzywniak SET QUANTITY = :wybraneWarzywoNowaIlosc WHERE NAME= :wybraneWarzywoNazwa")
+    void updateQuantityByName(String wybraneWarzywoNazwa, int wybraneWarzywoNowaIlosc);
+
+    @Query("SELECT COUNT(*) FROM Warzywniak") //Ile jest rekordów w tabeli
+    int size();
+}
